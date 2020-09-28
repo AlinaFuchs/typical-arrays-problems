f08d42e6993504351d9c4960dee818ff11e6b01a
Alina-Fuchs <kuzmenkoalina89@gmail.com>
2020-09-28 21:12:36 +0300

    feat:solved the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+if(!array || array.length === 0) {
+  return 0 
+} else {
+  let min = Math.min.apply(null, array)
+  return min
+}
+
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length === 0 ) {
+    return 0
+  } else {
+    let max = Math.max.apply(null, array)
+  return max
+  }
+
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if(!array || array.length === 0 ) {
+    return 0
+  } else {
+    let sum =0
+    for(let i = 0; i <array.length; i++) {
+      sum +=array[i];
+    } 
+    let avg = sum / array.length;
+    return avg
+   
+  }
+    
+ }
